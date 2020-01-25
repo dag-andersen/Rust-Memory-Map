e39ce38d0f4e024818a46a54d10e6de454ea751f
Dag Andersen <dagbjerreandersen@gmail.com>
2020-01-25 14:15:09 +0100

    Can now create a file and write/read

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use std::io::{BufRead, BufReader};
 use std::collections::HashMap;
 use memmap::{MmapMut, MmapOptions};
 use std::io::Read;
-
-
 use std::{
     fs::{OpenOptions, File},
     io::{Seek, SeekFrom, Write},
@@ -13,6 +11,15 @@ use std::{
     ptr,
 };
 use std::convert::TryInto;
+
+struct Node {
+    size: u128,
+    min_ip: u128,
+    max_ip: u128,
+    left: u128,
+    right: u128,
+    value: u128,
+}
 
 fn main() {
     let file = load_file("testdata/set1.txt");
@@ -40,21 +47,10 @@ fn interate(reader: BufReader<std::fs::File>, map: &mut HashMap<String,String>) 
     }
 }
 
-struct Node {
-    size: u128,
-    min_ip: u128,
-    max_ip: u128,
-    left: u128,
-    right: u128,
-    value: u128,
-}
-
 fn add_to_map(key: (&str,&str), value: &str, map: &mut HashMap<String, String>) {
     println!("- item added {} {}", key.0, value);
     map.insert(key.0.to_string(), value.to_string());
 }
-
-
 
 #[test]
 fn verify_simple_map() {
@@ -67,27 +63,43 @@ fn verify_simple_map() {
 
 #[test]
 fn test2() {
-    let test = std::mem::size_of::<Node>();
-    println!("{}", test);
+    let src = "hej";
 
     let mut mmap_options = MmapOptions::new();
-    let mut mmap: MmapMut = mmap_options.len(3).map_anon().unwrap();
-    mmap.copy_from_slice(b"hej");
+    let mut mmap: MmapMut = mmap_options.len(10).map_anon().unwrap();
+    mmap[..src.len()].copy_from_slice(src.as_bytes());
 
-    let test = &mmap[0..3];
+    let test = &mmap[0..4];
     print!("{}", std::str::from_utf8(test).unwrap());
-    
 
     assert!(true);
 }
 
-#[test]
-fn test() {
-    let file = File::open("README.md").unwrap();
-    let mmap = unsafe { MmapOptions::new().map( & file).unwrap() };
+fn get_memmap() -> MmapMut {
+    const SIZE: u64 = 128 * 128;
+    let src = "Hello!";
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open("testdata/db.txt")
+        .expect("Unable to open file");
+
+    file.seek(SeekFrom::Start(SIZE)).unwrap();
+    file.write_all(&[0]).unwrap();
+    file.seek(SeekFrom::Start(0)).unwrap();
+
+    let mut mmap = unsafe { MmapOptions::new().map_mut( & file).unwrap() };
     //let test = str:from_utf8_unchecked((& mmap[0..8]));
-    let test = &mmap[0..8];
+    mmap[..src.len()].copy_from_slice(src.as_bytes());
+    let test = &mmap[0..3];
     print!("{}", std::str::from_utf8(test).unwrap());
+    mmap
+}
+
+#[test]
+fn hejhej() {
+    get_memmap();
 }
 
 #[test]
@@ -100,7 +112,7 @@ fn test3() {
         .read(true)
         .write(true)
         .create(true)
-        .open("test.mmap")
+        .open("testdata/map.mmap")
         .expect("Unable to open file");
 
     // Allocate space in the file first
@@ -116,5 +128,7 @@ fn test3() {
 
     data[..src.len()].copy_from_slice(src.as_bytes());
 
+    let test = &data[0..8];
+    print!("{}", std::str::from_utf8(test).unwrap());
     assert!(true);
 }
