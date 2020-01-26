9c4ab7607170608db4ced14aac71e4cd999379f5
Dag Andersen <dagbjerreandersen@gmail.com>
2020-01-26 21:06:03 +0100

    Read Write with Structs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-
 const mapPath : &str = "testdata/db.txt";
 const sourcePath : &str = "testdata/set1.txt";
+const nodeSize : usize = std::mem::size_of::<Node>();
 
 use std;
 use std::io::{BufRead, BufReader};
@@ -8,21 +8,32 @@ use std::collections::HashMap;
 use memmap::{MmapMut, MmapOptions};
 use bytes::str::ByteStr;
 use std::io::Read;
-use std::{fs::{OpenOptions, File}, io::{Seek, SeekFrom, Write}, os::unix::prelude::AsRawFd, ptr, fs};
+use std::{fs::{OpenOptions, File}, io::{Seek, SeekFrom, Write}, os::unix::prelude::AsRawFd, ptr, fs, mem};
 use std::convert::TryInto;
+use bytes::MutBuf;
 
 struct Node {
-    size: u128,
-    min_ip: u128,
-    max_ip: u128,
-    left: u128,
-    right: u128,
-    value: u128,
+    //size: u32,
+    min_ip: u32,
+    max_ip: u32,
+    //    left: u128,
+//    right: u128,
+    name: u32,
 }
 
-fn main() {
-    run();
+unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
+    std::slice::from_raw_parts((p as *const T) as *const u8, std::mem::size_of::<T>())
 }
+
+fn node_from_bytes(slice: &[u8]) -> &Node { unsafe { bytes_to_typed(slice) } }
+
+pub unsafe fn bytes_to_typed<T>(slice: &[u8]) -> &T {
+    std::slice::from_raw_parts(slice.as_ptr() as *const T, std::mem::size_of::<T>())
+        .get(0)
+        .unwrap()
+}
+
+fn node_to_bytes(node: &Node) -> &[u8] { unsafe { any_as_u8_slice(node) } }
 
 fn run() {
     fs::remove_file(mapPath);
@@ -42,72 +53,103 @@ fn run() {
         let max = v[1];
         let com = v[2];
 
-        place_item(& mut mmap, & mut shifter, &com, &min, &max);
+        let node = Node {
+            min_ip: 20,
+            max_ip: 20,
+            name: 20,
+        };
+
+        place_item(& mut mmap, & mut shifter, &node);
     }
-}
 
-fn get_item(ip: &str) -> usize {
-
-    let mmap = get_memmap();
-    let byteMap = &mmap[0..100];
-    let s = std::str::from_utf8(byteMap).unwrap();
-    print!("{}", s);
-    s.find(ip).expect("didnt find")
-}
-
-#[test]
-fn get_works() {
-    run();
-
-    let offset = 27;
-    assert_eq!(offset+0, get_item("Net"));
-    assert_eq!(offset+3, get_item("com"));
-    assert_eq!(offset+19, get_item("Hans"));
+    print_map(&mmap);
 }
 
 fn place_item(
     mmap: & mut MmapMut,
     offset: & mut usize,
-    com: &str,
-    min: &str,
-    max: &str,
-) -> usize {
-    let mut s = com.to_owned();
-    s.push(' ');
-    s.push_str(&(min.to_owned()));
-    s.push(' ');
-    s.push_str(&(max.to_owned()));
-    s.push(' ');
+    node: & Node,
+) {
+    let bytes = node_to_bytes(node);
+    mmap[*offset..(*offset+bytes.len())].copy_from_slice(bytes);
+    println!("offset:{} {:?}", *offset, bytes);
+    println!("mmap: {:?}", &mmap[0..40]);
+    *offset += bytes.len();
+}
 
-    *offset += s.len();
+fn get_items<'a>(mmap: &'a MmapMut, ord: &str) -> &'a Node {
+    //let byteMap = &mmap[..];
+    let byteMap = unsafe { std::slice::from_raw_parts(mmap.as_ptr(), nodeSize) };
+    let node = node_from_bytes(&byteMap);
+    //print_map(&mmap);
+    println!("første dims: {:?}", &byteMap);
+    &node
+}
 
-    let test = *offset;
-
-    mmap[test..(test+s.len())].copy_from_slice(s.as_bytes());
-    s.len()
+fn print_map(map: &[u8]) {
+    let s = std::str::from_utf8(map).unwrap();
+    print!("{}", s);
 }
 
 fn get_buffer(name: &str) -> BufReader<std::fs::File> {
-    let file = File::open(name).expect("could not find file");
-    let reader = BufReader::new(file);
-    reader
+    BufReader::new(File::open(name).expect("could not find file"))
 }
 
 fn get_memmap() -> MmapMut {
     const SIZE: u64 = 128 * 128;
-
     let mut file = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
         .open(mapPath)
         .expect("Unable to open file");
-
     file.seek(SeekFrom::Start(SIZE)).unwrap();
     file.write_all(&[0]).unwrap();
     file.seek(SeekFrom::Start(0)).unwrap();
-
     let mut mmap = unsafe { MmapOptions::new().map_mut( & file).unwrap() };
-
     mmap
+}
+
+
+#[test]
+fn node_to_from_bytes() {
+    //let test = "a".as_bytes();
+
+    //let lort = u32::from_be_bytes(test);
+
+    let col = Node { min_ip: 10, max_ip: 20, name: 20 };
+    let bytes = unsafe { any_as_u8_slice(&col) };
+    println!("{:?}", bytes);
+    println!("{}", bytes.len());
+    assert_eq!(bytes, [10, 0, 0, 0, 20, 0, 0, 0, 20, 0, 0, 0]);
+    let node = node_from_bytes(bytes);
+    assert_eq!(20, node.name)
+}
+
+
+#[test]
+fn get_works() {
+    assert!(true);
+    println!("hej");
+    fs::remove_file(mapPath);
+    let mut mmap = get_memmap();
+    println!("hej");
+
+    assert!(true);
+
+    let node = Node {
+        min_ip: 20,
+        max_ip: 20,
+        name: 20,
+    };
+
+    println!("{:?}", node_to_bytes(&node));
+
+    place_item(& mut mmap, &mut 0, &node);
+
+
+    let offset = 27;
+    let getnode = get_items(&mmap, "Net");
+    println!("mmap: {:?}", &mmap[0..40]);
+    assert_eq!(getnode.name, 20)
 }
