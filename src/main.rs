a0b3f0b38a00e3ebbb6dc852b00ffea4023351a9
Dag Andersen <dagbjerreandersen@gmail.com>
2020-02-01 18:14:27 +0100

    print_to_file and huge optimisations to treebuild

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@
 #![allow(unused_imports)]
 #![allow(unused_results)]
 
-const MAP_PATH: &str = "testdata/db.txt";
-const SOURCE_PATH_1: &str = "testdata/set1.txt";
-const SOURCE_PATH_2: &str = "testdata/set2.txt";
-const SOURCE_PATH_3: &str = "testdata/set3.txt";
+const MAP_PATH: &str = "testdata/out/map.txt";
+const SOURCE_PATH_1: &str = "testdata/in/set1.txt";
+const SOURCE_PATH_2: &str = "testdata/in/set2.txt";
+const SOURCE_PATH_3: &str = "testdata/in/set3.txt";
+const TREE_PRINT_PATH: &str = "testdata/out/tree.txt";
 const NODE_SIZE : usize = std::mem::size_of::<Node>();
 
 use std::io::{BufRead, BufReader, LineWriter};
@@ -71,6 +72,9 @@ fn store_scr_on_map(scr: &str) {
 
     let mut mmap = get_memmap();
 
+    let ip_regex = Regex::new(r"(\d{1,3}[.]){3}(\d{1,3})").unwrap();
+    let name_regex = Regex::new(r"\b(([A-z]|\d)+\s?)+\b").unwrap();
+
     let buffer = get_buffer(scr);
 
     for (i, line) in buffer.lines().enumerate() {
@@ -78,7 +82,7 @@ fn store_scr_on_map(scr: &str) {
         let l = line.unwrap();
         if l.is_empty() { continue; }
 
-        let node = get_node_for_line(l);
+        let node = get_node_for_line(&ip_regex,&name_regex,l);
 
         if node.is_none() { continue }
         let node = node.unwrap();
@@ -87,9 +91,8 @@ fn store_scr_on_map(scr: &str) {
     }
 }
 
-fn get_node_for_line(l: String) -> Option<Node> {
-    let ip_regex = Regex::new(r"(\d{1,3}[.]){3}(\d{1,3})").unwrap();
-    let name_regex = Regex::new(r"\b(([A-z]|\d)+\s?)+\b").unwrap();
+fn get_node_for_line(ip_regex: &Regex, name_regex: &Regex, l: String) -> Option<Node> {
+
     let min_ip_match = ip_regex.find(l.as_bytes()).expect("didnt find min ip");
     let max_ip_match = ip_regex.find_at(l.as_bytes(), min_ip_match.end()).expect("didnt find max ip");
     let name_match = name_regex.find_at(l.as_bytes(), max_ip_match.end()).expect("didnt find name");
@@ -107,7 +110,7 @@ fn get_node_for_line(l: String) -> Option<Node> {
 
 fn get_u32_for_ip(v: &str ) -> Option<u32> {
     let v: Vec<&str> = v.split('.').collect();
-    if v.len() < 4 { return None }
+    if v.len() != 4 { return None }
     let mut min_array: [u8; 4] = Default::default();
     for i in 0..v.len() {
         min_array[i] = match v[i].parse() {
@@ -119,39 +122,39 @@ fn get_u32_for_ip(v: &str ) -> Option<u32> {
     Some(u32::from_be_bytes(min_array))
 }
 
-fn insert_node(mmap: & mut MmapMut, offset: usize, node: &Node) {
-    place_item(mmap, offset, &node);
-    if offset == 0 { return }
+fn insert_node(mmap: & mut MmapMut, index: usize, node: &Node) {
+    place_item(mmap, index, &node);
+    if index == 0 { return }
     let root = get_node(&mmap, 0);
-    insert_node_on_node(&mmap, root, offset, &node);
-    print!("-{}",offset);
+    insert_node_on_node(&mmap, root, index, &node);
+    //print!("-{}",offset);
 }
 
-fn insert_node_on_node(mmap: & MmapMut, parent: &mut Node, offset: usize, child: &Node) {
+fn insert_node_on_node(mmap: & MmapMut, parent: &mut Node, index: usize, child: &Node) {
 
     let mut offset_from_node = 0;
 
     if parent.min_ip <= child.min_ip && child.max_ip <= parent.max_ip {
-        println!("Overlap: {}", std::str::from_utf8(&child.name).unwrap());
+        println!("Overlap: {}", std::str::from_utf8(&child.name).expect("Overlap expect"));
         return
     }
 
     if parent.max_ip < child.max_ip {
         if parent.right == 0 {
-            parent.right = offset;
+            parent.right = index;
             return;
         }
         offset_from_node = parent.right;
     } else if parent.min_ip > child.min_ip {
         if parent.left == 0 {
-            parent.left = offset;
+            parent.left = index;
             return;
         }
         offset_from_node = parent.left;
     }
 
     let node= get_node(&mmap, offset_from_node);
-    insert_node_on_node(mmap, node, offset, &child);
+    insert_node_on_node(mmap, node, index, &child);
 }
 
 fn get_node<'a>(mmap: &'a MmapMut, index: usize) -> &'a mut Node {
@@ -173,28 +176,13 @@ fn find_node_in_tree(ip: u32) -> Option<[u8; 32]> {
         if accNode.min_ip <= ip && ip <= accNode.max_ip { return Some(accNode.name) }
 
         if accNode.max_ip < ip {
-            if accNode.right == 0 {
-                break;
-            }
+            if accNode.right == 0 { break; }
             offset_from_node = accNode.right;
         } else if accNode.min_ip > ip {
-            if accNode.left == 0 {
-                break;
-            }
+            if accNode.left == 0 { break; }
             offset_from_node = accNode.left;
         }
         accNode = get_node(&mmap, offset_from_node);
-    }
-    None
-}
-
-fn find_node(ip: u32) -> Option<[u8; 32]> {
-    let mut counter: usize = 0;
-    let mut mmap = get_memmap();
-    while counter < 50 {
-        let node = get_node(&mmap, counter);
-        if node.min_ip < ip && ip < node.max_ip { return Some(node.name) }
-        counter += 1;
     }
     None
 }
@@ -206,10 +194,6 @@ fn place_item(mmap: & mut MmapMut, index: usize, node: & Node) {
 fn place_item_raw(mmap: & mut MmapMut, offset: usize, node: & Node,) {
     let bytes = node_to_bytes(node);
     mmap[offset..(offset+bytes.len())].copy_from_slice(bytes);
-    //mmap.flush();
-    //mmap.flush_range(offset, bytes.len());
-    //println!("{:p}",&mmap[offset]);
-    //println!("{}", node)
 }
 
 fn get_buffer(file: &str) -> BufReader<std::fs::File> {
@@ -240,32 +224,70 @@ fn print_tree() {
 fn print_node(mmap: &MmapMut, node: &Node, n: usize) {
     let indention : String = (0..n).map(|_| '-').collect();
     if node.right != 0 {
-        let rightNode = get_node(&mmap, node.right);
-        print_node(mmap, &rightNode, n + 1);
+        print_node(mmap, &get_node(&mmap, node.right), n + 1);
     }
     print!("{}",indention);
     println!("{}", std::str::from_utf8(&node.name).unwrap());
     if node.left != 0 {
-        let leftNode = get_node(&mmap, node.left);
-        print_node(mmap, &leftNode, n + 1);
+        print_node(mmap, &get_node(&mmap, node.left), n + 1);
+    }
+}
+
+fn print_tree_to_file(s: &str) {
+    let file = File::create(s).unwrap();
+    let mut file = LineWriter::new(file);
+    let mmap = get_memmap();
+    let root = get_node(&mmap, 0);
+    print_node_to_file(&mmap, &root, 0, &mut file);
+}
+
+fn print_node_to_file(mmap: &MmapMut, node: &Node, n: usize, writer: &mut LineWriter<File>) {
+    if node.right != 0 {
+        print_node_to_file(mmap, &get_node(&mmap, node.right), n + 1, writer);
+    }
+    let indention : String = (0..n).map(|_| '-').collect();
+    writer.write_all(indention.as_bytes());
+    writer.write_all(&node.name);
+    writer.write_all("\n".as_bytes());
+    if node.left != 0 {
+        print_node_to_file(mmap, &get_node(&mmap, node.left), n + 1, writer);
     }
 }
 
 #[test]
-fn test_print_tree() {
+fn test_print_tree_to_file() {
     let src = "thisFileWillBeDeleted";
-    generate_source_file(200, src);
+    generate_source_file(10000, src);
     store_scr_on_map(src);
-    print_tree();
+    print_tree_to_file(TREE_PRINT_PATH);
     fs::remove_file(src);
 }
 
 #[test]
-fn test_make_40_file() {
-    let src = "40gb";
-    generate_source_file(1000, src);
+fn test_find_random() {
+    let src = "test_find_random";
+    let numberOfLines = 100;
+    generate_source_file(numberOfLines, src);
     store_scr_on_map(src);
-    print_tree();
+
+    let mut name: [u8; 32] = Default::default();
+    insert_array_in_array(& mut name, "testname".as_bytes());
+
+    let ip = 34568;
+
+    let node = Node { min_ip: ip-1, max_ip: ip+1, left: 0, right: 0, name, };
+
+    let mut mmap = get_memmap();
+    insert_node(&mut mmap,numberOfLines, &node);
+
+    let getNode = find_node_in_tree(ip);
+    assert!(getNode.is_some());
+    let getNode = getNode.unwrap();
+    let left = std::str::from_utf8(&name).unwrap();
+    let right = std::str::from_utf8(&getNode).unwrap();
+    assert_eq!(left, right);
+
+    fs::remove_file(src);
 }
 
 #[test]
@@ -292,7 +314,6 @@ fn test_get_ip_for_line() {
 #[test]
 fn test_find_node_in_tree() {
     store_scr_on_map(SOURCE_PATH_1);
-    let mut mmap = get_memmap();
 
     let name = find_node_in_tree(get_u32_for_ip("000.000.000.015").unwrap());
     assert!(name.is_some());
@@ -313,52 +334,6 @@ fn test_find_node_in_tree() {
     assert!(name.is_none());
 }
 
-#[test]
-fn test_find() {
-    fs::remove_file(MAP_PATH);
-
-    let mut name: [u8; 32] = Default::default();
-    insert_array_in_array(& mut name, "name".as_bytes());
-
-    let min_ip = 20;
-    let max_ip = 40;
-
-    let node1 = Node { min_ip, max_ip: 0, left: 0, right: 0, name: Default::default(), };
-    let node2 = Node { min_ip, max_ip: 40, left: 0, right: 0, name, };
-    let node3 = Node { min_ip, max_ip: 0, left: 0, right: 0, name: Default::default(), };
-
-    let mut first_map = get_memmap();
-    place_item(& mut first_map, 0, &node1);
-    place_item(& mut first_map, 1, &node2);
-    place_item(& mut first_map, 2, &node3);
-
-    let mut another_map = get_memmap();
-    let temp = find_node((min_ip+max_ip)/2);
-    assert!(temp.is_some());
-    let gotten_name = find_node((min_ip+max_ip)/2).unwrap();
-
-    assert_eq!(gotten_name, name);
-}
-
-#[test]
-fn test_place_item_and_get() {
-    fs::remove_file(MAP_PATH);
-    let mut name: [u8; 32] = Default::default();
-    insert_array_in_array(& mut name, "name".as_bytes());
-
-    let node = Node { min_ip: 20, max_ip: 20, left: 0, right: 0, name: name, };
-
-    let mut first_map = get_memmap();
-    place_item(& mut first_map, 0, &node);
-
-    let mut another_map = get_memmap();
-    let getnode = get_node(&another_map, 0);
-
-    let left = std::str::from_utf8(&name).unwrap();
-    let right = std::str::from_utf8(&getnode.name).unwrap();
-
-    assert_eq!(left, right);
-}
 
 #[test]
 fn test_correct_placement() {
@@ -381,27 +356,6 @@ fn test_correct_placement() {
     assert_eq!(left, right);
 }
 
-#[test]
-#[should_panic]
-fn test_correct_placement_panic() {
-    fs::remove_file(MAP_PATH);
-
-    let mut name: [u8; 32] = Default::default();
-    insert_array_in_array(& mut name, "name".as_bytes());
-
-    let node = Node { min_ip: 20, max_ip: 20, left: 0, right: 0, name: name, };
-
-    let mut first_map = get_memmap();
-    place_item(& mut first_map, 0, &node);
-
-    let mut another_map = get_memmap();
-    //get something that doesnt exists
-    let getnode = get_node(&another_map, 1);
-
-    let left = std::str::from_utf8(&name).unwrap();
-    let right = std::str::from_utf8(&getnode.name).unwrap();
-    assert_eq!(left, right);
-}
 
 fn generate_random_ip_firm() -> String {
     let mut rng = rand::thread_rng();
@@ -418,7 +372,7 @@ fn generate_random_ip_firm() -> String {
     r.push_str(&format!("{}",ip2)); r.push('.');
     r.push_str(&format!("{}",ip3)); r.push('.');
     r.push_str(&format!("{}",ip4)); r.push(' ');
-    let name = rng.sample_iter(&Alphanumeric).take(10).collect::<String>();
+    let name = rng.sample_iter(&Alphanumeric).take(4).collect::<String>();
     r.push_str(&name); r.push_str("\n");
     r
 }
@@ -427,11 +381,10 @@ fn generate_source_file(n: usize, s:&str) {
     let file = File::create(s).unwrap();
     let mut file = LineWriter::new(file);
     for i in 0..n {
-        if i % 100 == 0 { println!("number of lines created: {}", i); }
+        if i % 1000 == 0 { println!("number of lines created: {}", i); }
         let s = generate_random_ip_firm();
         file.write_all( s.as_bytes());
         file.flush();
-        //print!("{}", s);
     }
 }
 
