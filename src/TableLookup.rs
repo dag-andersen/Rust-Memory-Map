3eb08a37155c02d5f7def10e46a372f037173d45
Dag Andersen <dagbjerreandersen@gmail.com>
2020-02-19 20:32:37 +0100

    Changed name size to be 1 byte.

--- src/TableLookup.rs
+++ src/TableLookup.rs
@@ -1,16 +1,16 @@
-use crate::{Utils, usizeSize};
+use crate::{Utils, usizeSize, u8Size};
 use memmap::{MmapMut, MmapOptions};
 
 pub fn place_name(mmap: &mut MmapMut, offset: usize, name: &[u8]) -> usize {
     let len = name.len();
-    Utils::place_item_raw(mmap, offset, &len);
-    mmap[offset+usizeSize..offset+usizeSize+len].copy_from_slice(name);
-    offset + usizeSize + len
+    //mmap[offset..offset+u8Size].copy_from_slice(&[len as u8]);
+    Utils::place_item_raw(mmap, offset, &(len as u8));
+    mmap[offset+u8Size..offset+u8Size+len].copy_from_slice(name);
+    offset + u8Size + len
 }
 
-pub fn get_name(mmap: &MmapMut, offset: usize) {
-    let namesize: usize = unsafe { *Utils::bytes_to_type(&mmap[offset..(offset+usizeSize)]) };
-    let nameAsBytes = &mmap[offset+usizeSize..offset+usizeSize+namesize];
-    let name = std::str::from_utf8(nameAsBytes).expect("bad formatting");
-    println!("crazy test: {}",name);
+pub fn get_name(mmap: &MmapMut, offset: usize) -> &str {
+    let name_size: u8 = unsafe { *Utils::bytes_to_type(&mmap[offset..(offset+u8Size)]) };
+    let nameAsBytes = &mmap[offset+u8Size..offset+u8Size+(name_size as usize)];
+    std::str::from_utf8(nameAsBytes).expect("bad formatting")
 }
