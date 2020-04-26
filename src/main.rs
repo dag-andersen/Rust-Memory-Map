2a9dd868291dcb6b716480ed8edf95e73232510c
Dag Andersen <dagbjerreandersen@gmail.com>
2020-04-26 19:47:19 +0200

    Now you can search for specific ip

--- src/main.rs
+++ src/main.rs
@@ -44,12 +44,11 @@ mod RedBlack;
 mod Table;
 mod IntegrationTests;
 mod BenchmarkTest;
-//mod BenchmarkTests_Separate;
 mod Utils;
 mod PayloadMap;
 
 use std::io::{LineWriter, Error, Lines};
-use std::ops::Add;
+use std::ops::{Add, Range};
 use memmap::{MmapMut, MmapOptions};
 use std::io::Read;
 use std::{fs::{OpenOptions, File}, io::{Seek, SeekFrom, Write}, os::unix::prelude::AsRawFd, ptr, fs, mem, fmt, io};
@@ -60,11 +59,12 @@ use std::io::prelude::*;
 use rand::distributions::Alphanumeric;
 use rand::prelude::ThreadRng;
 use std::iter::{Map, FilterMap, Filter, FromIterator, Enumerate};
-use crate::BenchmarkTest::{build_and_search_data_structures, input_data_shuffled, input_data, shuffle_in_momory, range, create_table, shuffle_file, create_redblack, create_BST, search_time_BST, search_time_redblack, search_time_table};
+use crate::BenchmarkTest::{create_table, shuffle_file, create_redblack, create_BST, search_time_BST, search_time_redblack, search_time_table};
 use clap::{Arg, App, SubCommand};
-use std::process::Command;
+use std::process::{Command, exit};
 use std::thread::sleep;
 use core::time;
+use crate::Utils::get_u32_for_ip;
 
 pub struct Entry {
     pub min_ip: u32,
@@ -78,11 +78,142 @@ impl fmt::Display for Entry {
     }
 }
 
-pub static mut number_of_entries: u32 = 150_000_000;
+pub const input_data:               &str = "input_data.txt";
+pub const input_data_shuffled:      &str = "input_data_shuffled.txt";
+
+pub static mut number_of_entries:    u32 = 150_000_000;
+pub const range:              Range<u32> = 10..18;
+pub const shuffle_in_momory:        bool = false;
 
 fn main() {
     Utils::make_needed_folders();
-    build_and_search_data_structures();
+
+
+    let matches = App::new("Rust Memory Map")
+        .version("0.1.0")
+        .author("Dag Andersen <dagbjerreandersen@gmail.com>")
+        .about("Searching in memory mapped files")
+        .arg(Arg::with_name("number_of_entries")
+            .short("n")
+            .long("number_of_entries")
+            .takes_value(true)
+            .help("The number of entries"))
+        .arg(Arg::with_name("payload_size")
+            .short("p")
+            .long("payload_size")
+            .takes_value(true)
+            .help("The amount of bytes for each entry"))
+        .arg(Arg::with_name("gap_size")
+            .short("g")
+            .long("gap_size")
+            .takes_value(true)
+            .help("The number of entries it skips while selecting/collecting entries to search for"))
+        .arg(Arg::with_name("input_file")
+            .short("i")
+            .long("input_file")
+            .takes_value(true)
+            .help("The file for building the data-structure"))
+        .arg(Arg::with_name("specific_ip")
+            .short("s")
+            .long("specific_ip")
+            .takes_value(true)
+            .help("The specific ip you want to search for"))
+        .arg(Arg::with_name("generate_data")
+            .long("generate_data")
+            .help("Generates random entries instead of getting the input from a file"))
+        .arg(Arg::with_name("print_info")
+            .long("print_info")
+            .help("Prints the setup for this run"))
+        .arg(Arg::with_name("build_table")
+            .long("build_table")
+            .help("Builds a Table for given input"))
+        .arg(Arg::with_name("build_BST")
+            .long("build_BST")
+            .help("Builds a BST for given input"))
+        .arg(Arg::with_name("build_redblack")
+            .long("build_redblack")
+            .help("Builds a Redblack Tree for given input"))
+        .arg(Arg::with_name("search_table")
+            .long("search_table")
+            .help("Searches the Table with <number_of_entries / gap_size> number of entries"))
+        .arg(Arg::with_name("search_BST")
+            .long("search_BST")
+            .help("Searches down the BST with <number_of_entries / gap_size> number of entries"))
+        .arg(Arg::with_name("search_redblack")
+            .long("search_redblack")
+            .help("Searches down the Redblack Tree with <number_of_entries / gap_size> number of entries"))
+        .get_matches();
+
+    let shuffled_file_str = matches.value_of("input_file");
+
+    let n = match matches.value_of("number_of_entries") {
+        None => 150_000_000,
+        Some(s) => s.parse::<u32>().unwrap_or(150_000_000)
+    };
+    unsafe { number_of_entries = n }
+
+    let piece = (std::u32::MAX as f64 / n as f64) as u32;
+    let padding = piece - range.end..piece - range.start;
+
+    let payload_size = match matches.value_of("payload_size") {
+        None => 50,
+        Some(s) => s.parse::<usize>().unwrap_or(50)
+    };
+
+    let gap_size = match matches.value_of("gap_size") {
+        None => 10,
+        Some(s) => s.parse::<usize>().unwrap_or(10)
+    };
+
+    if matches.is_present("print_info") {
+        println!("\nHOSTNAME: {}", String::from_utf8(Command::new("hostname").output().unwrap().stdout).unwrap());
+        println!("Benchmark input: n: {}, range: {:#?}, padding: {:#?}, payload_size: {}, gap:{} \n\n", &n, &range, &padding, payload_size, &gap_size);
+    }
+
+    let input = match shuffled_file_str {
+        Some(f) => f,
+        None if matches.is_present("generate_data") => {
+            fs::remove_file(input_data);
+            fs::remove_file(input_data_shuffled);
+            if shuffle_in_momory {
+                FileGenerator::generate_source_file_shuffled(input_data, n, range, padding, payload_size);
+            } else {
+                FileGenerator::generate_source_file(input_data, n, range, padding, payload_size);
+                sleep(time::Duration::from_secs(1));
+                shuffle_file(input_data, input_data_shuffled);
+            }
+            input_data_shuffled
+        },
+        _ => {
+            println!("You have to specify a input file or add the flag --generate_data");
+            exit(0)
+        }
+    };
+
+    if matches.is_present("build_BST") { create_BST(input); }
+    if matches.is_present("build_redblack") { create_redblack(input); }
+    if matches.is_present("build_table") { create_table(input); }
+
+    if matches.is_present("search_BST") {
+        match matches.value_of("specific_ip") {
+            None => println!("{}",search_time_BST(input,gap_size)),
+            Some(s) => println!("{}",BST::find_value(get_u32_for_ip(s).expect("Invalid IP")).unwrap_or("Nothing found".to_string()))
+        };
+    }
+
+    if matches.is_present("search_redblack") {
+        match matches.value_of("specific_ip") {
+            None => println!("{}",search_time_redblack(input,gap_size)),
+            Some(s) => println!("{}",RedBlack::find_value(get_u32_for_ip(s).expect("Invalid IP")).unwrap_or("Nothing found".to_string()))
+        };
+    }
+
+    if matches.is_present("search_table") {
+        match matches.value_of("specific_ip") {
+            None => println!("{}",search_time_table(input,gap_size)),
+            Some(s) => println!("{}",Table::find_value(get_u32_for_ip(s).expect("Invalid IP")).unwrap_or("Nothing found".to_string()))
+        };
+    }
 }
 
 fn build_data_structure(input: &str, payload_path: &str, structure: MmapMut, inserter: fn(&mut MmapMut, usize, Entry, u64)) {
